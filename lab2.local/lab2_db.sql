1b4144b5b4248c1371bb7b50c84c634990dae9bc
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-02 05:49:07 +0700

    2 new

--- lab2.local/lab2_db.sql
+++ lab2.local/lab2_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.2
--- Время создания: Дек 01 2024 г., 07:47
+-- Время создания: Дек 02 2024 г., 05:48
 -- Версия сервера: 8.2.0
 -- Версия PHP: 8.1.28
 
@@ -32,6 +32,8 @@ CREATE TABLE `users` (
   `Login` varchar(255) NOT NULL,
   `Password` char(64) NOT NULL,
   `Email` varchar(255) NOT NULL,
+  `BirthDate` date DEFAULT NULL,
+  `ProgrammingLanguage` varchar(255) DEFAULT NULL,
   `ActivationToken` varchar(64) DEFAULT NULL,
   `IsActive` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -40,9 +42,8 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`ID`, `Login`, `Password`, `Email`, `ActivationToken`, `IsActive`) VALUES
-(5, 'ilya_barsukov', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 'barsuk@mail.ru', NULL, 1),
-(6, '11', '4fc82b26aecb47d2868c4efbe3581732a3e7cbcc6c2efb32062c08170a05eeb8', '11@mail.ru', '41a8f3198d1100627c014e88f0589c6f', 0);
+INSERT INTO `users` (`ID`, `Login`, `Password`, `Email`, `BirthDate`, `ProgrammingLanguage`, `ActivationToken`, `IsActive`) VALUES
+(10, 'ilya_barsukov', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 'barsuk@nemail.ru', '2000-04-24', 'YoptaScript', NULL, 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -63,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- lab2.local/register.php
+++ lab2.local/register.php
@@ -5,16 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
+    $birthdate = $_POST['birthdate'];
+    $language = $_POST['language'];
 
     $passwordHash = hash('sha256', $password);
     $activationToken = bin2hex(random_bytes(16));
 
     try {
-        $stmt = $pdo->prepare("INSERT INTO users (Login, Password, Email, IsActive, ActivationToken) VALUES (:username, :password, :email, 0, :token)");
+        $stmt = $pdo->prepare("INSERT INTO users (Login, Password, Email, BirthDate, ProgrammingLanguage, IsActive, ActivationToken) 
+                               VALUES (:username, :password, :email, :birthdate, :language, 0, :token)");
         $stmt->execute([
             'username' => $username,
             'password' => $passwordHash,
             'email' => $email,
+            'birthdate' => $birthdate,
+            'language' => $language,
             'token' => $activationToken,
         ]);
 
@@ -30,12 +35,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <label for="username">Логин:</label>
     <input type="text" name="username" required>
     <br>
+
     <label for="password">Пароль:</label>
     <input type="password" name="password" required>
     <br>
+
     <label for="email">Email:</label>
     <input type="email" name="email" required>
     <br>
+
+    <label for="birthdate">Дата рождения:</label>
+    <input type="date" name="birthdate" required>
+    <br>
+
+    <label for="language">Изучаемый язык программирования:</label>
+    <select name="language" required>
+        <option value="Python">Python</option>
+        <option value="JavaScript">JavaScript</option>
+        <option value="YoptaScript">YoptaScript</option>
+        <option value="Java">Java</option>
+        <option value="C++">C++</option>
+        <option value="PHP">PHP</option>
+        <option value="Other">Другой</option>
+    </select>
+    <br>
+
     <button type="submit">Зарегистрироваться</button>
 </form>
-
